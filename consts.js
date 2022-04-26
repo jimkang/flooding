7a74b769182f20e022501c42a61cf6b4a36174db
Jim Kang <jimkang@gmail.com>
2022-04-25 20:52:45 -0400

    Don't go to full denseness during every single period.

--- consts.js
+++ consts.js
@@ -1,10 +1,6 @@
 export const bigTempoWaveAmp = 0.15;
 export const densificationPeriod = 30;
-export const totalTicks = densificationPeriod;
+export const totalPeriods = 10;
+export const totalTicks = densificationPeriod * totalPeriods;
 export const timeNeededForEnvelopeDecay = 2.0;
-
-// At about 60 bpm, the 16-beat riff takes up about a
-// quarter of a minute.
-
 export const diamondLimit = 15;
-
--- updaters/get-chord.js
+++ updaters/get-chord.js
@@ -1,8 +1,12 @@
-import { densificationPeriod, tonalityDiamondPitches } from '../consts';
+import { densificationPeriod, totalTicks } from '../consts';
+import { tonalityDiamondPitches } from '../tonality-diamond';
 
 var descendingMode = false;
 
 export function getChord({ ticks }) {
+  // TODO: Try something less linear.
+  const densificationCoeff = ticks / totalTicks;
+
   var periodTick = ticks % densificationPeriod;
   // Tick down in descendingMode, tick up otherwise.
   if (descendingMode) {
@@ -13,9 +17,8 @@ export function getChord({ ticks }) {
   }
   
   // TODO: Don't go all the way to max denseness every single time. 
-  // Ride up a little bit, then go more each time.
-  const denseness = periodTick/densificationPeriod % densificationPeriod;
-  const chordPitchCount = Math.round(denseness * tonalityDiamondPitches.length);
+  const denseness = (periodTick/densificationPeriod % densificationPeriod) * densificationCoeff;
+  const chordPitchCount = Math.round(denseness * tonalityDiamondPitches.length) || 1;
   console.log('chordPitchCount', chordPitchCount);
   // TODO: Slightly off start times?
   return { rates: tonalityDiamondPitches.slice(0, chordPitchCount) };
