dd683c3ba01c0eb601fafdb741e65a22fd3dfce1
Jim Kang <jimkang@gmail.com>
2022-07-19 15:56:47 -0400

    Fix issues with undoer handing out references instead of copies so that the history values were getting written over and with the first undo call not actually undoing.

--- updaters/undoer.js
+++ updaters/undoer.js
@@ -1,41 +1,51 @@
 import { range } from 'd3-array';
 
+// The object constructed here assumes that it's going to be working on an array value.
 export function Undoer({
   historyLimit = 200,
   onUpdateValue,
   storageKey,
 }) {
-  var pastValues = [range(800).map(() => 0)];
+  var history = [range(800).map(() => 0)];
   var storedJSON = localStorage.getItem(storageKey);
   if (storedJSON) {
-    pastValues[0] = JSON.parse(storedJSON);
+    history[0] = JSON.parse(storedJSON);
   }
+  console.log('history length:', history.length);
   var undoer = { onChange, onUndo, getCurrentValue };
   return undoer;
 
   function onChange(newValue) {
-    pastValues.push(newValue);
-    if (pastValues.length > historyLimit) {
-      pastValues.shift();
+    //if (history.length > 0 && history[history.length - 1].every((x, i) => x === newValue[i])) {
+    //console.log('newValue is the same as the last one.');
+    //return;
+    //}
+    history.push(newValue);
+    if (history.length > historyLimit) {
+      history.shift();
     }
+    console.log('history length:', history.length);
     localStorage.setItem(storageKey, JSON.stringify(newValue));
   }
 
   function onUndo() {
-    if (pastValues.length < 2) {
+    if (history.length < 2) {
       return;
     }
-    // TODO: Avoid "going back" to the most recent change, which is sort
-    // of not undoing at all.
-    var prevValue = pastValues.pop();
+    history.pop();
+    var prevValue = history[history.length - 1];
     if (prevValue) {
-      onUpdateValue(prevValue, undoer);
+      // Return a copy so that things don't edit things in our history.
+      onUpdateValue(prevValue.slice(), undoer);
+      localStorage.setItem(storageKey, JSON.stringify(prevValue));
     }
+    console.log('history length:', history.length);
   }
 
   function getCurrentValue() {
-    if (pastValues.length > 0) {
-      return pastValues[pastValues.length - 1];
+    if (history.length > 0) {
+      // Return a copy so that things don't edit things in our history.
+      return history[history.length - 1].slice();
     }
   }
 }
