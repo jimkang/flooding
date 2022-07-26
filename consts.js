a98c967451c78baa160397a2af3a3ffed4672ce4
Jim Kang <jimkang@gmail.com>
2022-07-25 21:03:41 -0400

    Add missing const.

--- consts.js
+++ consts.js
@@ -1,4 +1,5 @@
 export const defaultTotalTicks = 80;
+export const defaultSecondsPerTick = 1;
 export const timeNeededForEnvelopeDecay = 5.0;
 export const diamondLimit = 15;
 export const maxTickLength = 4.0; // In seconds.
