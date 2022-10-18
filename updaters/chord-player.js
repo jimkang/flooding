4c2902df707bea5cbcab38e14cf3c8eff0bfa5b6
Jim Kang <jimkang@gmail.com>
2022-10-17 20:57:38 -0400

    Got rid of looping on sampler which caused bad clipping sound but may make long notes too short.

--- updaters/chord-player.js
+++ updaters/chord-player.js
@@ -25,7 +25,7 @@ export function ChordPlayer({ ctx, sampleBuffer }) {
         {      
           sampleBuffer,
           playbackRate: rate,
-          loop: true,
+          loop: false,
           timeNeededForEnvelopeDecay: currentTickLengthSeconds
         }
       );
