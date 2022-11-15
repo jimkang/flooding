514b4ead41631be0d475cca54029edfc20169fe9
Jim Kang <jimkang@gmail.com>
2022-11-14 22:12:53 -0500

    Try some extreme scaling.

--- updaters/data-director.js
+++ updaters/data-director.js
@@ -1,11 +1,12 @@
 import { tonalityDiamondPitches } from '../tonality-diamond';
 import { range } from 'd3-array';
-import { scaleLinear } from 'd3-scale';
+//import { scaleLinear } from 'd3-scale';
+import { scalePow } from 'd3-scale';
 
 const maxPitchCount = tonalityDiamondPitches.length;
 
 export function DataDirector({ tempoFactor = 1, data, chordProp, chordXFloor, chordXCeil }) {
-  var chordScale = scaleLinear().domain([chordXFloor, chordXCeil]).range([1, maxPitchCount]);
+  var chordScale = scalePow().exponent(50).domain([chordXFloor, chordXCeil]).range([1, maxPitchCount]);
   var index = 0;
 
   return { getChord, getTickLength };
