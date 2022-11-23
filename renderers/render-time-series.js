e4f7098c60ce79935e063e60fe4e0e6429e05de5
Jim Kang <jimkang@gmail.com>
2022-11-22 19:42:31 -0500

    Switch to rendering with SVG, which seems to get rid of misrepresenting spikes near the end.

--- renderers/render-time-series.js
+++ renderers/render-time-series.js
@@ -1,40 +1,33 @@
 import { scaleLinear } from 'd3-scale';
+import { select } from 'd3-selection';
 
 export function RenderTimeSeries({ canvasId, color = 'green' }) {
   var canvas = document.getElementById(canvasId);
-  var canvasCtx = canvas.getContext('2d', { alpha: false });
-  canvasCtx.lineWidth = 1;
   const width = +canvas.getAttribute('width');
   const height = +canvas.getAttribute('height');
+  var root = select(canvas).select('.root');
 
   return renderTimeSeriesCanvas;
 
   function renderTimeSeriesCanvas({
-    valueOverTimeArray, totalTime, valueMin = 0, valueMax, currentTick
+    valueOverTimeArray, totalTime, valueMin = 0, valueMax//, currentTick
   }) {
     var x = scaleLinear().domain([0, totalTime]).range([0, width]);
     var y = scaleLinear().domain([valueMin, valueMax]).range([0, height]);
-    requestAnimationFrame(drawValues);
 
-    function drawValues() {
-      canvasCtx.clearRect(0, 0, width, height);
-      canvasCtx.fillStyle = color;
-  
-      var barX = 0;
-      valueOverTimeArray.forEach(drawTimeSpan);
+    var bars = root.selectAll('.event-bar').data(valueOverTimeArray);
+    bars.exit().remove();
+    var newBars = bars.enter().append('rect');
+    var currentBars = bars.merge(newBars);
+    currentBars
+      .attr('width', ed => x(ed.time))
+      .attr('height', ed => y(ed.value))
+      .attr('transform', getBarTransform);
 
-      function drawTimeSpan({ time, value }, i) {
-        const barWidth = x(time);
-        const barHeight = y(value);
-        if (i === currentTick) {
-          canvasCtx.fillStyle = 'hsl(60, 100%, 50%)';
-        }
-        canvasCtx.fillRect(barX, height - barHeight, barWidth, barHeight);
-        if (i === currentTick) {
-          canvasCtx.fillStyle = color;
-        }
-        barX += barWidth;
-      }
+    function getBarTransform(ed, i) {
+      const left = i * x(ed.time);
+      const top = height - y(ed.value);
+      return `translate(${left}, ${top})`;
     }
   }
 }
