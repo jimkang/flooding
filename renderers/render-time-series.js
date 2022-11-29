761b6ab07d87422eb80776d62c936a80d496f235
Jim Kang <jimkang@gmail.com>
2022-11-28 21:12:58 -0500

    Add zoom.

--- renderers/render-time-series.js
+++ renderers/render-time-series.js
@@ -1,11 +1,15 @@
 import { scaleLinear } from 'd3-scale';
 import { select } from 'd3-selection';
+import { zoomIdentity, zoom as Zoom } from 'd3-zoom';
+
+var zoom;
 
 export function RenderTimeSeries({ canvasId, color = 'green' }) {
-  var canvas = document.getElementById(canvasId);
-  const width = +canvas.getAttribute('width');
-  const height = +canvas.getAttribute('height');
-  var root = select(canvas).select('.root');
+  var canvas = select(document.getElementById(canvasId));
+  const width = +canvas.attr('width');
+  const height = +canvas.attr('height');
+  var root = canvas.select('.root');
+  wireZoom({ x: 0, y: 0, k: 1.0 });
 
   return renderTimeSeriesCanvas;
 
@@ -34,4 +38,17 @@ export function RenderTimeSeries({ canvasId, color = 'green' }) {
       return valueOverTimeArray.slice(0, i).reduce((total, ed) => ed.time + total, 0);
     }
   }
+ 
+  function wireZoom({ x, y, k }) {
+    zoom = Zoom()
+      .scaleExtent([1, 32])
+      .on('zoom', zoomed);
+    canvas.call(zoom.transform, zoomIdentity.translate(x, y).scale(k));
+    canvas.call(zoom);
+ 
+    function zoomed(zoomEvent) {
+      root.attr('transform', zoomEvent.transform);
+    }
+  }
 }
+
