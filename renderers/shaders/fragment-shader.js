23d114eafe8eb170f1df355276ead0d486ed2b6f
Jim Kang <jimkang@gmail.com>
2025-07-23 18:01:13 -0400

    Diagonal.

--- renderers/shaders/fragment-shader.js
+++ renderers/shaders/fragment-shader.js
@@ -112,12 +112,11 @@ float wave(float x, float y, float t, float density, float wiggle, float yAdjust
 float waveLine(float x, float y, float t, float density, float wiggle,
   float yAdjust, float lineBlur, float lineThickness) {
 
-  float frequency = 1.;
   float outY = wave(x, y, t, density, wiggle, yAdjust);
-  outY += sin(x*frequency*02.1 + t)*.45;
-  outY += sin(x*frequency*01.72 + t*1.121)*.40;
-  outY += sin(x*frequency*02.221 + t*0.437)*.50;
-  outY += sin(x*frequency*03.1122+ t*4.269)*.25;
+  outY += sin(16. * x + t);
+  // outY += sin(x*frequency*01.72 + t*1.121)*.040;
+  // outY += sin(x*frequency*02.221 + t*0.437)*.050;
+  // outY += sin(x*frequency*03.1122+ t*4.269)*.025;
 
   // outY = repeatedNoise(5, 4., .1, outY) + yAdjust;
   // outY += .01 * fract(sin(2000. * outY));
