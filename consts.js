e59cdef1c5dddd00fd22b2c39ed34e052145a99d
Jim Kang <jimkang@gmail.com>
2025-11-21 16:03:38 -0500

    Brought a bunch of stuff back, shortened each tick.

--- consts.js
+++ consts.js
@@ -1,5 +1,5 @@
 import { getTonalityDiamond } from './tonality-diamond';
-export const defaultSecondsPerTick = 20;
+export const defaultSecondsPerTick = 10;
 export const diamondLimit = 11;
 
 const denomLimit = 32;
