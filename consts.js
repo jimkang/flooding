0e009313546240664870dfdc33c3a7b256870fcf
Jim Kang <jimkang@gmail.com>
2025-12-27 12:15:05 -0500

    Adjust maxTempo.

--- consts.js
+++ consts.js
@@ -3,4 +3,4 @@ export const timeNeededForEnvelopeDecay = 5.0;
 export const diamondLimit = 15;
 export const maxTickLength = 4.0; // In seconds.
 export const defaultSecondsPerTick = 0.5;
-export const maxTempo = 200;
+export const maxTempo = 16;
